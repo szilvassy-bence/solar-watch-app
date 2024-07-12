6484e896b40c06f444941d5c3160e9c67110427b
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-12 13:54:00 +0200

    Created SunriseSunset.cs model

---
+++ backend/backend/Models/SunriseSunset.cs
@@ -0,0 +1,24 @@
+namespace backend.Models;
+
+public class SunriseSunset
+{
+    public int Id { get; set; }
+    
+    public DateTime Sunrise { get; set; }
+
+    public DateTime Sunset { get; set; }
+
+    public DateOnly Day => DateOnly.FromDateTime(Sunrise);
+
+    // called foreign key property
+    public int CityId { get; set; }
+    
+    // navigation property
+    public City City { get; set; }
+
+    public override string ToString()
+    {
+        return
+            $"{nameof(Id)}: {Id}, {nameof(Sunrise)}: {Sunrise}, {nameof(Sunset)}: {Sunset}, {nameof(CityId)}: {CityId}";
+    }
+}
