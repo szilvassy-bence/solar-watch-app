8ee91c199fb1cedb6b176be48e90f09b4786a458
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 12:00:14 +0200

    created cities route in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import Root from './layout/root';
-
-import Home from './pages/home';
 import Error from './pages/error'
+import Home from './pages/home';
+import Cities from './pages/cities';
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root/>,
     errorElement: <Error/>,
     
     children: [
         {index: true, element: <Home/>},
+        {
+          path: '/cities',
+          element: <Cities />
+        }
 
     ]
   }
