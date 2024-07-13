464d0ed661c74bd96e3b47ce9fe5584fab166f24
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 11:05:19 +0200

    created a new method for retrieving all cities from database

--- backend/backend/Repositories/CityRepository/CityRepository.cs
+++ backend/backend/Repositories/CityRepository/CityRepository.cs
@@ -1,23 +1,44 @@
+using backend.Data;
 using backend.Models;
 using backend.Services;
 using backend.Services.CityProvider;
 using backend.Services.JsonProcessor;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Repositories.CityRepository;
 
 public class CityRepository : ICityRepository
 {
+    private readonly ILogger<CityRepository> _logger;
     private readonly ICityProvider _cityProvider;
     private readonly IJsonProcessor _jsonProcessor;
+    private readonly SolarContext _context;
 
-    public CityRepository(ICityProvider cityProvider, IJsonProcessor jsonProcessor)
+    public CityRepository(
+        ILogger<CityRepository> logger,
+        ICityProvider cityProvider, 
+        IJsonProcessor jsonProcessor, 
+        SolarContext context
+        )
     {
+        _logger = logger;
         _cityProvider = cityProvider;
         _jsonProcessor = jsonProcessor;
+        _context = context;
+    }
+
+    public async Task<IEnumerable<City>> GetCities()
+    {
+        var cities = await _context.Cities
+            .ToListAsync();
+        _logger.LogInformation("Cities asked from the database.");
+        return cities;
     }
 
     public async Task<City> GetCity(string city)
     {
+        
         string cityJson = await _cityProvider.GetCity(city);
         return _jsonProcessor.ProcessCity(cityJson);
     }
--- backend/backend/Repositories/CityRepository/ICityRepository.cs
+++ backend/backend/Repositories/CityRepository/ICityRepository.cs
@@ -4,5 +4,6 @@ namespace backend.Repositories.CityRepository;
 
 public interface ICityRepository
 {
+    public Task<IEnumerable<City>> GetCities();
     public Task<City> GetCity(string city);
 }
