e16c79820ec9164644eb1d05aefb6aa533044e57
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 08:10:21 +0200

    updated vite.config.js and eslint to proxy the server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_BACKEND_URL || "http://localhost:5120/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 })
