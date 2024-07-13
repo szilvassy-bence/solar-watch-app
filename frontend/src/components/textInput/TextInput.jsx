67d7f3b81603f8957aeac5d11ea902f06b73c9be
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 08:21:17 +0200

    updated TextInput component to handle change event

--- frontend/src/components/textInput/TextInput.jsx
+++ frontend/src/components/textInput/TextInput.jsx
@@ -1,12 +1,13 @@
 import TextField from '@mui/material/TextField';
 
-const TextInput = ({ label, defaultValue, helperText }) => {
+const TextInput = ({ label, defaultValue, helperText, onChange }) => {
 	return (
 		<TextField
           id="outlined-error-helper-text"
           label={label}
           defaultValue={defaultValue}
           helperText={helperText}
+          onChange={onChange}
         />
 	)
 }
