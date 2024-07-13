ab9f0a735430df885a70be23948a0d446b26d283
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 10:26:28 +0200

    created SolarContext.cs

---
+++ backend/backend/Data/SolarContext.cs
@@ -0,0 +1,45 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class SolarContext : DbContext
+{
+    public SolarContext(DbContextOptions<SolarContext> options) : base(options)
+    {}
+    
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunriseSunset> SunriseSunsets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<City>()
+            .HasIndex(c => c.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<City>()
+            .HasMany(c => c.SunriseSunsets)
+            .WithOne(ss => ss.City)
+            .HasForeignKey(ss => ss.CityId);
+        
+        modelBuilder.Entity<City>()
+            .HasData(
+                new City
+                {
+                    Id = 1, Name = "London", Latitude = 51.509865, Longitude = -0.118092, Country = "GB",
+                    State = "England"
+                },
+                new City
+                {
+                    Id = 2, Name = "Budapest", Latitude = 47.497913, Longitude = 19.040236, Country = "HU"
+                },
+                new City
+                {
+                    Id = 3, Name = "Paris", Latitude = 48.864716, Longitude = 2.349014, Country = "FR",
+                    State = "Ile-de-France"
+                }
+            );
+    }
+}
