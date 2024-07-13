674bf8299be1fa58ead8be037b5aadd15e180fc5
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-13 12:00:02 +0200

    updated Program.cs to retrieve env variable of DB_HOST if exists and use that as conn. string

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -18,13 +18,16 @@ builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
 builder.Services.AddSingleton<ICityProvider, CityProvider>();
 builder.Services.AddScoped<ICityRepository, CityRepository>();
 
-var sqlServerConnectionString = builder.Configuration.GetConnectionString("SqlServerDefault");
+var dbHost = System.Environment.GetEnvironmentVariable("DATABASE_HOST");
+// in docker we set this, otherwise default to localhost
+dbHost ??= "localhost";
+var dbConnectionString = $"Server={dbHost},1433;Database=Solar;User Id=sa;Password=Solar-Watch-2024;Encrypt=false;";
 AddDbContext();
 
 void AddDbContext()
 {
     builder.Services.AddDbContext<SolarContext>(options =>
-        options.UseSqlServer(sqlServerConnectionString));
+        options.UseSqlServer(dbConnectionString));
 }
 
 var isTestEnvironment = Environment.GetEnvironmentVariable("IS_TEST_ENVIRONMENT");
@@ -40,6 +43,56 @@ else
 }
 
 var app = builder.Build();
+
+ApplyMigrations();
+
+async void ApplyMigrations()
+{
+    using var scope = app.Services.CreateScope();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    const int maxRetries = 6;
+    int retries = 0;
+    bool dbReady = false;
+
+    while (!dbReady && retries < maxRetries)
+    {
+        try
+        {
+            var solarContext = scope.ServiceProvider.GetRequiredService<SolarContext>();
+            
+            logger.LogInformation("Try to connect and migrate with connection string: {connectionString}", dbConnectionString);
+
+            var canConnect = await solarContext.Database.CanConnectAsync();
+            if (canConnect)
+            {
+                logger.LogInformation("Could connect to database.");
+            }
+            else
+            {
+                logger.LogInformation("Could not connect to database.");
+            }
+
+            solarContext.Database.Migrate();
+
+            dbReady = true;
+            logger.LogInformation("Migrations applied successfully.");
+        }
+        catch (Exception ex)
+        {
+            retries++;
+            logger.LogWarning(ex,
+                "Database not ready. Waiting before retrying... Attempt {Attempt} of {MaxRetries}", retries,
+                maxRetries);
+            await Task.Delay(TimeSpan.FromSeconds(Math.Pow(2, retries))); // Exponential backoff
+        }
+    }
+
+    if (!dbReady)
+    {
+        logger.LogError("Failed to apply migrations after {MaxRetries} attempts.", maxRetries);
+    }
+}
 
 app.Logger.LogInformation(0, "The application is started.");
 
