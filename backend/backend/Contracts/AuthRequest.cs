e44d5e758b7a4ce7ea36a282ac6cb64d56aa9a2d
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-17 08:48:47 +0200

    created AuthRequest.cs and AuthResponse.cs

---
+++ backend/backend/Contracts/AuthRequest.cs
@@ -0,0 +1,3 @@
+namespace backend.Contracts;
+
+public record AuthRequest(string Email, string Password);
---
+++ backend/backend/Contracts/AuthResponse.cs
@@ -0,0 +1,3 @@
+namespace backend.Contracts;
+
+public record AuthResponse(string Email, string UserName, string Token, string Role);
