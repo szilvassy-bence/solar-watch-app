bf106aba10c092cb1be0ebb97e4fa83c27785c9d
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-18 14:35:50 +0200

    Updated TextInput.jsx

--- frontend/src/components/textInput/TextInput.jsx
+++ frontend/src/components/textInput/TextInput.jsx
@@ -1,13 +1,16 @@
 import TextField from '@mui/material/TextField';
 
-const TextInput = ({ label, defaultValue, helperText, onChange }) => {
+const TextInput = ({ label, type, defaultValue, helperText, onChange, required, error }) => {
 	return (
 		<TextField
           id="outlined-error-helper-text"
           label={label}
+          type={type ?? "text"}
           defaultValue={defaultValue}
-          helperText={helperText}
           onChange={onChange}
+          required={required}
+          helperText={helperText}
+          error={error}
         />
 	)
 }
